776b8fc2f4704dfb3b72a144c939960ff33428b2
Oswaldo Ravelo <raveloswaldo@gmail.com>
2022-11-25 20:56:41 -0300

    sets the methods for dashboard component

--- Tour-of-heroes/src/app/dashboard/dashboard.component.ts
+++ Tour-of-heroes/src/app/dashboard/dashboard.component.ts
@@ -21,7 +21,7 @@ export class DashboardComponent implements OnInit {
 
 
   getHeroes():void{
-    this.heroService.getsHero()
+    this.heroService.getHeroes()
     .subscribe(heroes=>this.heroes=heroes.slice(1,5))
   }
 }
