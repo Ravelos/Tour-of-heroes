71e21d2daf14756c00c3099fa9b6a483bc0f1db6
Oswaldo Ravelo <raveloswaldo@gmail.com>
2022-11-25 21:01:21 -0300

    adapts the functions for this component

--- Tour-of-heroes/src/app/heroes/heroes.component.ts
+++ Tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,8 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
-import { MessageService } from '../message.service';
-
 
 @Component({
   selector: 'app-heroes',
@@ -10,28 +9,16 @@ import { MessageService } from '../message.service';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
+  heroes: Hero[] = [];
 
-  heroes:Hero []= [];
-  selectedHero?: Hero;
-
-  constructor(
-    private heroService: HeroService,
-    private messageService:MessageService
-  ) { }
+  constructor(private heroService: HeroService) { }
 
   ngOnInit(): void {
-    this.getsHeroes();
+    this.getHeroes();
   }
 
-  onSelect(hero:Hero): void{
-    this.selectedHero = hero;
-    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
+  getHeroes(): void {
+    this.heroService.getHeroes()
+    .subscribe(heroes => this.heroes = heroes);
   }
-
-
-  getsHeroes():void{
-    this.heroService.getsHero()
-    .subscribe(heroes=>this.heroes =heroes);
-  }
-
 }
